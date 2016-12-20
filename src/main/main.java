af98e5f08f9b1abfcc9e44a58e1620d13c9e6ea9
Anna Katalin Kis <Katalin.Kis@msg-systems.com>
2016-12-20 09:48:21 +0200

    Test from Kati

--- src/main/main.java
+++ src/main/main.java
@@ -3,7 +3,7 @@ package main;
 public class main {
 
 	public static void main(String[] args) {
-		System.out.println("Our git repo ;)");
+		//System.out.println("Our git repo ;)");
 		System.out.println("Kati");
 	}
 
