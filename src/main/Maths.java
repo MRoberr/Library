21d07f717ef8e85aee63155be5fbd527ed0437b5
Anna Katalin Kis <Katalin.Kis@msg-systems.com>
2016-12-20 13:05:27 +0200

    Added new class

--- src/main/Maths.java
+++ src/main/Maths.java
@@ -1,6 +1,7 @@
 package main;
 
 public class Maths {
+	
 	public static int pow(int x, int y){
 		if (x > 1 && y > 20){
 			return -1;
--- src/main/main.java
+++ src/main/main.java
@@ -5,17 +5,15 @@ public class main {
 	public static void main(String[] args) {
 
 		//System.out.println("Our git repo ;)");
-<<<<<<< HEAD
+
 		System.out.println("Kati - new branch");
 		
-=======
 		System.out.println("Kati");
 
 		System.out.println("Our git repo ;)");
 		System.out.println("sjozsef");
 		
 		System.out.println("Zolti");
->>>>>>> branch 'master' of https://github.com/MRoberr/Library.git
 	}
 
 }
