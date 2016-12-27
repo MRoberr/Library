40ea933178d78b5c6cec28375b267edb7175111b
Simo Zoltan <simoz@MSGN04475.int.root.msg.ag>
2016-12-27 13:51:52 +0200

    Loc Comm

--- library/library_client/src/main/java/edu/msg/library_client/desktop/ClientMain.java
+++ library/library_client/src/main/java/edu/msg/library_client/desktop/ClientMain.java
@@ -1,9 +1,8 @@
 package edu.msg.library_client.desktop;
 
-public class ClientMain {
-
-	public static void main(String[] args) {
-		ClientService.initClient(args);
+public class ClientMain {	
+	
+	public static void main(String[] args) {				
+		ClientService.initClient();
 	}
-
 }
