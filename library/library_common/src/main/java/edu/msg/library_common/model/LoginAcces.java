4f48f1c5defc374a015abd896e89e82ab97aa585
Nagy Zoltan <zoltan.nagy@msg-sytems.com>
2016-12-27 10:40:59 +0200

    Megnezten

---
+++ library/library_common/src/main/java/edu/msg/library_common/model/LoginAcces.java
@@ -0,0 +1,5 @@
+package edu.msg.library_common.model;
+
+public enum LoginAcces {
+	ADMIN, USER, DENIED
+}
--- library/library_server/src/main/java/edu/msg/library_server/backend/ServerMain.java
+++ library/library_server/src/main/java/edu/msg/library_server/backend/ServerMain.java
@@ -1,10 +1,12 @@
 package edu.msg.library_server.backend;
 
+import edu.msg.library_common.model.LoginAcces;
 import edu.msg.library_server.backend.service.ServerService;
 
 public class ServerMain {
 
 	public static void main(String[] args) {
-		ServerService.initServer();;
+		System.out.println(LoginAcces.ADMIN.toString());
+		ServerService.initServer();
 	}
 }
