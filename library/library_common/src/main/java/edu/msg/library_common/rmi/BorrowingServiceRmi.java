8d31c8b92084ee90e617be5252f3c2a220109b6a
gallb <barna.gall@msg-systems.com>
2016-12-27 15:59:59 +0200

    modifyed BorrowingService.java BorrowingServiceRmi.java

--- library/library_common/src/main/java/edu/msg/library_common/rmi/BorrowingServiceRmi.java
+++ library/library_common/src/main/java/edu/msg/library_common/rmi/BorrowingServiceRmi.java
@@ -2,6 +2,9 @@ package edu.msg.library_common.rmi;
 
 import java.rmi.Remote;
 import java.rmi.RemoteException;
+import java.util.List;
+
+import edu.msg.library_common.model.Borrowing;
 import edu.msg.library_common.model.Entity;
 
 public interface BorrowingServiceRmi extends Remote {
@@ -10,21 +13,43 @@ public interface BorrowingServiceRmi extends Remote {
 
 	public static final int RMI_PORT = 1099;
 	
-//	public List<Entity> getAllPublications() throws RemoteException; 
+	/**
+	 * 
+	 * @return list of Entities containing Borrow objects
+	 * @throws RemoteException
+	 */
+	
+	public List<Entity> getAllBorrows() throws RemoteException;
 	
 	/**
 	 * 
-	 * @param ent - Borrowing object
+	 * @param borrow - Borrowing object
 	 * @return true if the insert was successful, or false if not
 	 * @throws RemoteException
 	 */
-	public boolean insertPublication(Entity ent) throws RemoteException;
+	public boolean insertPublication(Borrowing borrow) throws RemoteException;
 	
 	/**
 	 * 
-	 * @param ent - Borrowing object
+	 * @param borrow - Borrowing object
 	 * @return true if the insert was successful, or false if not
 	 * @throws RemoteException
 	 */
-	public boolean updatePublication(Entity ent) throws RemoteException;
+	public boolean updatePublication(Borrowing borrow) throws RemoteException;
+	
+	/**
+	 * 
+	 * @param borrow - Borrowing object
+	 * @return true if the delete was successful, or false if not
+	 * @throws RemoteException
+	 */
+	public boolean deleteBook(Borrowing borrow) throws RemoteException;
+	
+	/**
+	 * 
+	 * @param uuid - the unique identifier of the given borrow
+	 * @return returns a Borrow object
+	 * @throws RemoteException
+	 */
+	public Entity getBorrowByUUID(String uuid) throws RemoteException; 
 }
