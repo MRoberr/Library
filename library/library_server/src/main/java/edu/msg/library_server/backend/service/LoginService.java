df48a1f37695cd176cd3ea6c096177d67aa6ac62
Nagy Zoltan <zoltan.nagy@msg-sytems.com>
2016-12-27 11:05:34 +0200

    Commit1

--- library/library_server/src/main/java/edu/msg/library_server/backend/service/LoginService.java
+++ library/library_server/src/main/java/edu/msg/library_server/backend/service/LoginService.java
@@ -8,21 +8,24 @@ import java.util.List;
 import edu.msg.library_common.model.Entity;
 import edu.msg.library_common.model.LoginAccess;
 import edu.msg.library_common.rmi.LoginServiceRmi;
+import edu.msg.library_server.backend.repository.SqlHandler;
 
 public class LoginService extends UnicastRemoteObject implements LoginServiceRmi {
 
 	private static final long serialVersionUID = 1L;
 
 	protected LoginService() throws RemoteException {
-		super();		
+		super();
 	}
 
-	@Override 
+	@Override
 	public LoginAccess login(String user, String password) throws RemoteException {
-		//
+		return SqlHandler.getInstance().executeLoginSelect(user, password);
+	}
+
+	public synchronized String logout() throws RemoteException {
+		// return SqlHandler.getInstance().executeLogOut();
 		return null;
 	}
-
-
 
 }
