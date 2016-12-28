8d2fc887e827083fc8b600fe7b8f5616d88ea99e
Anna Katalin Kis <Katalin.Kis@msg-systems.com>
2016-12-28 11:08:49 +0200

    added ' symbol to insert methods

--- library/library_common/src/main/java/edu/msg/library_common/model/Magazin.java
+++ library/library_common/src/main/java/edu/msg/library_common/model/Magazin.java
@@ -78,7 +78,7 @@ public class Magazin extends BaseEntity {
 	@Override
 	public String getInsert() {
 		return "insert into magazines (uuid, title, article_title, publisher, release_date, nr_of_copies, copies_left)"
-				+ "values(" + getUUID() + ",'" + this.title + "','" + this.article_title + "','" + this.publisher
+				+ "values('" + getUUID() + "','" + this.title + "','" + this.article_title + "','" + this.publisher
 				+ "','" + this.releaseDate + "'," + this.numberOfCopies + "," + this.copiesLeft + ")";
 	}
 
