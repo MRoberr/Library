4bd5d892616cf3fa2cb2c252fbc16d9d2c7f5c3a
Jozsef Solomon <Jozsef.Solomon@msg-systems.com>
2016-12-28 11:39:35 +0200

    commit

--- library/library_common/src/test/java/edu/msg/library_common/model/NewspaperTest.java
+++ library/library_common/src/test/java/edu/msg/library_common/model/NewspaperTest.java
@@ -27,7 +27,7 @@ public class NewspaperTest {
 		System.out.println("insert: " + insert);
 		assertEquals(
 				"insert into newspapers (uuid, title, article_title, publisher, release_date, nr_of_copies, copies_left)"
-						+ "values(456456,'newspaper','cikk','publisher','2015-12-01',5,6)",
+						+ "values('456456','newspaper','cikk','publisher','2015-12-01',5,6)",
 				insert);
 	}
 
