25c0dba862bcb36505cd08105c277097f582e9bc
MRoberr <majairober@gmail.com>
2017-01-03 09:49:23 +0200

    UiFactory reseted csak 1 sort kell módosítani ahhoz, hogy más ui induljon

--- library/library_client/src/main/java/edu/msg/library_client/desktop/UiFactory.java
+++ library/library_client/src/main/java/edu/msg/library_client/desktop/UiFactory.java
@@ -1,14 +1,14 @@
 package edu.msg.library_client.desktop;
 
-import edu.msg.library_client.desktop.GUI.control.MainController;
 //import edu.msg.library_client.desktop.console.MainConsole;
+import edu.msg.library_client.desktop.console.MainConsole;
 
 public abstract class UiFactory {
 
-	public static UiFactory getUiFatory() {
+	public static void getUi() {
 		
-		return new MainController();
-//		return new MainConsole();
+//		new MainController();
+		new MainConsole().startConsole();
 		
 	}
 }
