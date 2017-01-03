5bb292b079bc98b0bdd09628f6c71a5d274fab03
Sipos Terez <terez.sipos@msg-systems.com>
2017-01-03 15:37:48 +0200

    Search publication by Regexp edited

--- library/library_server/src/main/java/edu/msg/library_server/backend/service/SearchService.java
+++ library/library_server/src/main/java/edu/msg/library_server/backend/service/SearchService.java
@@ -8,7 +8,7 @@ import edu.msg.library_common.model.Publication;
 import edu.msg.library_common.rmi.SearchServiceRmi;
 import edu.msg.library_server.backend.repository.SqlHandler;
 
-public class SearchService extends UnicastRemoteObject implements SearchServiceRmi{
+public class SearchService extends UnicastRemoteObject implements SearchServiceRmi {
 
 	private static final long serialVersionUID = 1L;
 
@@ -17,14 +17,18 @@ public class SearchService extends UnicastRemoteObject implements SearchServiceR
 	}
 
 	@Override
-	public List<Publication> searchPublicationByTitles(String title) {		
-		
-		return SqlHandler.getInstance().executePublicationSelect(Publication.getSelectByTitle(title));		
+	public List<Publication> searchPublicationByTitles(String title) {
+		return SqlHandler.getInstance().executePublicationSelect(Publication.getSelectByTitle(title));
 	}
-	
-	
-	public List<Publication> searchPublicationByUUID(String uuid) {		
-		
-		return SqlHandler.getInstance().executePublicationSelect(Publication.getSelectByPublicationUUID(uuid));		
+
+	public List<Publication> searchPublicationByUUID(String uuid) {
+		return SqlHandler.getInstance().executePublicationSelect(Publication.getSelectByPublicationUUID(uuid));
+	}
+
+	@Override
+	public List<Publication> searchPublicationByRegexp(String regexp) throws RemoteException {
+		String reg=regexp.replace("+", "[a-zA-Z0-9 ]+");
+		reg=reg.replace("*", "[a-zA-Z0-9 ]*");
+		return SqlHandler.getInstance().executePublicationSelect(Publication.getSelectByRegexp(reg));
 	}
 }
