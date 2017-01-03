e7db053ff84a4386200790a5599002f36117515d
MRoberr <majairober@gmail.com>
2017-01-03 10:27:12 +0200

    UiFactory is now OK

--- library/library_client/src/main/java/edu/msg/library_client/desktop/UiFactory.java
+++ library/library_client/src/main/java/edu/msg/library_client/desktop/UiFactory.java
@@ -1,14 +1,14 @@
 package edu.msg.library_client.desktop;
 
-//import edu.msg.library_client.desktop.console.MainConsole;
-import edu.msg.library_client.desktop.console.MainConsole;
+import edu.msg.library_client.desktop.jfxgui.controller.MainWindowController;
+import javafx.application.Application;
 
 public abstract class UiFactory {
 
 	public static void getUi() {
 		
-//		Application.launch(MainWindowController.class);
-		new MainConsole().startConsole();
+		Application.launch(MainWindowController.class);
+//		new MainConsole().startConsole();
 		
 	}
 }
