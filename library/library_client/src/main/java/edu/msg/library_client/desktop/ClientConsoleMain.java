b4fcec73a226fed1e4faf7d9a69635dbcdc390c5
Nagy Zoltan <zoltan.nagy@msg-sytems.com>
2017-01-04 09:49:45 +0200

    ClienConsoleMain commit

---
+++ library/library_client/src/main/java/edu/msg/library_client/desktop/ClientConsoleMain.java
@@ -0,0 +1,11 @@
+package edu.msg.library_client.desktop;
+import java.rmi.NotBoundException;
+
+public class ClientConsoleMain {	
+	
+	public static void main(String[] args) throws NotBoundException {				
+		
+		UiFactory.getConsole();
+	
+	}
+}
--- library/library_client/src/main/java/edu/msg/library_client/desktop/UiFactory.java
+++ library/library_client/src/main/java/edu/msg/library_client/desktop/UiFactory.java
@@ -1,13 +1,20 @@
 package edu.msg.library_client.desktop;
 
 import edu.msg.library_client.desktop.console.MainConsole;
+import edu.msg.library_client.desktop.jfxgui.controller.MainWindowController;
+import javafx.application.Application;
 
 public abstract class UiFactory {
 
 	public static void getUi() {
-		
-//		Application.launch(MainWindowController.class);
+
+		Application.launch(MainWindowController.class);
+
+	}
+
+	public static void getConsole() {
+
 		new MainConsole().startConsole();
-		
+
 	}
 }
