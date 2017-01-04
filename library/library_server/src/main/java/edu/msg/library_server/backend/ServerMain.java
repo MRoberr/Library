b8c494e24492095f8f71af1c8a4f26adaccc94ed
Simo Zoltan <simoz@MSGN04475.int.root.msg.ag>
2017-01-04 16:00:30 +0200

    SL4J logging with Maven initialiazed.

--- library/library_server/src/main/java/edu/msg/library_server/backend/ServerMain.java
+++ library/library_server/src/main/java/edu/msg/library_server/backend/ServerMain.java
@@ -1,10 +1,15 @@
 package edu.msg.library_server.backend;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import edu.msg.library_server.backend.service.ServerService;
 
 public class ServerMain {
+	private static final Logger LOGGER = LoggerFactory.getLogger(ServerMain.class);
 
-	public static void main(String[] args) {		
+	public static void main(String[] args) {	
+		
+		LOGGER.error("Server start LOG!");
 		ServerService.initServer();
 	}
 }
